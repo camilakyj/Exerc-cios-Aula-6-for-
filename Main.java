ba33f39754d4ba09ea332d6113c0ba68bd18ae70
Camila <yagimaj@gmail.com>
2026-03-28 17:28:54 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+// Camila Aki Yagima Jeronimo
+package dobackofront;
+import java.io.InputStream;
+import java.util.Scanner;
+public class Main {
+    public    static void main(String[] args) {
+
+       Scanner entrada = new Scanner(System.in);
+
+        for (int cont = 100; cont >= 1; cont-- ) {
+            System.out.print(cont + " ");
+
+        }
+
+
+    }
+
+ }
+

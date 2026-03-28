d23a6384471531b440742fd309fe4bd029b1c279
Camila <yagimaj@gmail.com>
2026-03-28 19:35:20 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+// Camila Aki Yagima Jeronimo
+package dobackofront;
+import java.util.Scanner;
+public class Main {
+public    static void main(String[] args) {
+
+
+    for (int numero = 15; numero <= 100; numero++) {
+        int quadrado = numero * numero;
+
+        System.out.println("O quadrado de " + numero + " é " + quadrado);
+    }
+
+
+}}
+
+
+
